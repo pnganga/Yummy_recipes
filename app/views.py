e306ca09ce9e174c25428e306e9e3cd640e320ac
pnganga <pnganga05@gmail.com>
2017-10-23 12:50:33 +0300

    running app using gunicorn for heroku

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, request, session
 from app import app
 from .forms import LoginForm, SignUpForm, NewRecipeForm
-import simulated_models 
+form app import simulated_models 
 from app.ensure_loggedin import ensure_logged_in
 
 
